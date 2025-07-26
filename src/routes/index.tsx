d5cc0e743bdb9fd7bc7022dfd707d8ce5d0b9806
AmadeonSilveira <kasilveira32@gmail.com>
2025-07-26 13:15:13 -0300

    config de rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from '../screens/Home';
+
+export type routesList = {
+    Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<routesList>();
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
